5eb97ad58aefed21fca3b03099e58de46246c8ca
Edison <jedisoncortes@gmail.com>
2021-06-13 22:13:19 -0500

    Button

--- app/src/main/java/com/df/whiskyhunter/WhiskyDetail.java
+++ app/src/main/java/com/df/whiskyhunter/WhiskyDetail.java
@@ -2,6 +2,8 @@ package com.df.whiskyhunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -51,6 +53,10 @@ public class WhiskyDetail extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        
+        if (v.getId() == R.id.btnVisite) {
+            Uri uri = Uri.parse(link);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
     }
 }
